947910df5ddfcaed424526af698fa2f0b7ab7e26
Julie Moroz <julie.prokasheva@gmail.com>
2024-12-02 18:21:09 +0300

    Assignment 2.

--- stem_framework/stem/core.py
+++ stem_framework/stem/core.py
@@ -2,11 +2,13 @@
 Core module.
 """
 from typing import Optional
+from dataclasses import dataclass
+import re
 
 
 def pascal_case_to_snake_case(name: str) -> str:
-    ... # TODO()
-
+    half_snake = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', half_snake).lower()
 
 
 class Named:
@@ -14,8 +16,14 @@ class Named:
 
     @property
     def name(self):
-        ... # TODO()
+        if self._name is not None:
+            return self._name
+        else:
+            name = self.__class__.__name__
+            print(name)
+            return pascal_case_to_snake_case(name)
 
 
+@dataclass
 class Dataclass:
     ...  # TODO()
--- stem_framework/stem/meta.py
+++ stem_framework/stem/meta.py
@@ -3,15 +3,17 @@ Metadata is data that provides information about other data.
 Metadata processor is a principle of data processing when no user instructions or manually managed
 intermediate states are possible. It allows using only data or metadata.
 """
-from dataclasses import dataclass
-from typing import Optional, Any, Union
+from dataclasses import dataclass, astuple, asdict
+import dataclasses
+from typing import Optional, Any, Union, Tuple
+from stem_framework.stem.core import Dataclass
 
 
-Meta = ... # TODO()
+Meta = Union[Dataclass, dict]
 
-SpecificationField = ... # TODO()
+SpecificationField = Tuple[str, Union[float, Tuple[Any], Meta]]
 
-Specification = ... # TODO()
+Specification = Union[Dataclass, Tuple[SpecificationField]]
 
 
 class SpecificationError(Exception):
@@ -30,19 +32,42 @@ class MetaVerification:
 
     def __init__(self, *errors: Union[MetaFieldError, "MetaVerification"]):
         self.error = errors
-        # TODO("checked_success")
+        self.checked_success = len(errors) == 0
 
     @staticmethod
-    def verify(meta: Meta,
-               specification: Optional[Specification] = None) -> "MetaVerification":
-        ...  # TODO()
+    def verify(meta: Meta, specification: Optional[Specification] = None) -> "MetaVerification":
+        errors = []
+        if dataclasses.is_dataclass(specification):
+            specification = tuple((key, type(value)) for key, value in specification.__dict__.items() if key[0] != '_')
+
+        for pair in specification:
+            (required_key, required_types) = pair
+            presented_type = type(get_meta_attr(meta, required_key))
+
+            if isinstance(required_types, type):
+                required_types = (required_types, )
+
+            if isinstance(required_types, tuple):
+                if presented_type not in required_types:
+                    error = MetaFieldError(required_key, required_types, presented_type)
+                    errors.append(error)
+            else:
+                verification = MetaVerification.verify(meta, specification)
+                if verification.checked_success:
+                    errors.append(verification)
+
+        return MetaVerification(*errors)
 
 
-def get_meta_attr(meta : Meta, key : str, default : Optional[Any] = None) -> Optional[Any]:
-    ...
-    # TODO()
+def get_meta_attr(meta: Meta, key: str, default: Optional[Any] = None) -> Optional[Any]:
+    if isinstance(meta, dict):
+        return meta.get(key, default)
+    else:
+        return getattr(meta, key, default)
 
 
 def update_meta(meta: Meta, **kwargs):
-    ...
-    # TODO()
+    if isinstance(meta, dict):
+        meta.update(kwargs)
+    for k, v in kwargs.items():
+        setattr(meta, k, v)
