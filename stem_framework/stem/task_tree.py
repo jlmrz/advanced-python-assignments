3a6a0629ea4ba2c0250d06a8c0d8b232a300a771
Julie Moroz <julie.prokasheva@gmail.com>
2024-12-13 17:51:19 +0300

    Assignment 5. Implements task tree

--- stem_framework/stem/task_tree.py
+++ stem_framework/stem/task_tree.py
@@ -7,26 +7,71 @@ T = TypeVar("T")
 
 
 class TaskNode(Generic[T]):
-    task: Task[T]
+    def __init__(self, task: Task, workspace: IWorkspace) -> None:
+        self.task = task
+        self._workspace = IWorkspace.find_default_workspace(task) if workspace is None else workspace
+        self._dependencies = TaskNode.set_resolved(task, self._workspace)
+        self._unresolved_dependencies = TaskNode.set_unresolved(task, self._workspace)
 
     @property
     def dependencies(self) -> list["TaskNode"]:
-        pass  # TODO()
+        return self._dependencies
+
+    @property
+    def workspace(self) -> IWorkspace:
+        return self._workspace
 
     @property
     def is_leaf(self) -> bool:
-        pass  # TODO()
+        return len(self._dependencies) == 0
 
     @property
     def unresolved_dependencies(self) -> list["str"]:
-        pass # TODO()
+        return self._unresolved_dependencies
 
     @property
     def has_dependence_errors(self) -> bool:
-        pass  # TODO()
+        return len(self.unresolved_dependencies) != 0
+
+    @staticmethod
+    def set_resolved(task: Task[T], workspace: IWorkspace) -> list["TaskNode"]:
+        _resolved = []
+        for taskpath in task.dependencies:
+            if workspace.has_task(taskpath):
+                _resolved.append(TaskNode(workspace.find_task(taskpath), workspace))
+        return _resolved
+
+    @staticmethod
+    def set_unresolved(task: Task[T], workspace: IWorkspace) -> list["str"]:
+        _unresolved_dependencies = []
+        for taskpath in task.dependencies:
+            if not workspace.has_task(taskpath):
+                _unresolved_dependencies.append(task.name)
+            elif workspace.has_task(taskpath) and len(task.dependencies) != 0:
+                task = workspace.find_task(taskpath)
+                _unresolved_dependencies.extend(TaskNode.set_unresolved(task, workspace))
+        return _unresolved_dependencies
 
 
 class TaskTree:
+    def __init__(self, task: Task[T], workspace: Optional[IWorkspace] = None) -> None:
+        self.root = TaskTree.build_node(task, workspace)
+
+    @staticmethod
+    def build_node(task: Task[T], workspace: Optional[IWorkspace] = None) -> TaskNode[T]:
+        return TaskNode(task, workspace)
+
+    def find_task(self, task: Task[T],  workspace: Optional[IWorkspace] = None) -> TaskNode[T]:
+        if task == self.root.task and workspace == self.root.workspace:
+            return self.root
+        else:
+            for dependency in self.root.dependencies:
+                subtree = TaskTree(dependency.task, dependency.workspace)
+                node = subtree.find_task(task, workspace)
+                if node is not None:
+                    return node
 
     def resolve_node(self, task: Task[T], workspace: Optional[IWorkspace] = None) -> TaskNode[T]:
-        pass  # TODO()
+        workspace = IWorkspace.find_default_workspace(task) if workspace is None else workspace
+        node = self.find_task(task, workspace)
+        return node
