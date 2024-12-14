777dfa0d76726e2357896c9311518359b03a25cb
Julie Moroz <julie.prokasheva@gmail.com>
2024-12-14 11:29:17 +0300

    Assignment 5. CLI is partly restored

--- stem_framework/setup.py
+++ stem_framework/setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-
 requirements = [
     'Sphinx~=8.1.3',
     'Pylint~=3.3.1',
@@ -10,11 +9,16 @@ requirements = [
 name = 'stem'
 setup(
     name=name,
+    version='0.1.0',
     install_requires=requirements,
-    packages=find_packages(),
-   # packages=['core.py', 'meta.py', 'task.py', 'workspace'],
+    packages=find_packages(include=['stem', 'stem.*']),
     command_options={
         'build_sphinx': {
             'project': ('setup.py', name),
-            'source_dir': ('setup.py', 'doc')}},
+            'source_dir': ('setup.py', '.')}},
+    entry_points={
+        'console_scripts': [
+            'stemcli = stem.cli_main:stem_cli_main',
+        ],
+    }
 )
--- stem_framework/stem/cli_main.py
+++ stem_framework/stem/cli_main.py
@@ -1,7 +1,6 @@
 import argparse
 import cmd
-
-from .workspace import IWorkspace
+from stem.workspace import IWorkspace
 
 
 def print_structure(workspace: IWorkspace, args: argparse.Namespace):
@@ -21,8 +20,32 @@ def run_task(workspace: IWorkspace, args: argparse.Namespace):
 
 
 def create_parser() -> argparse.ArgumentParser:
-    pass  # TODO()
+    parser = argparse.ArgumentParser(description='Run task in workspace')
+    parser.add_argument(
+        '-w', '--workspace', metavar='WORKSPACE',
+        help='Add path to workspace or file for module workspace',
+        required=True
+    )
+
+    subparsers = parser.add_subparsers(metavar='command')
+    structure_parser = subparsers.add_parser('structure', help='Print workspace structure')
+    structure_parser.set_defaults(func=print_structure)
+    run_parser = subparsers.add_parser('run', help='Run task')
+    run_parser.set_defaults(func=run_task)
+    run_parser.add_argument('TASKPATH')
+    run_parser.add_argument(
+        '-m', '--meta',
+        metavar='META', help='Metadata for task or path to file with metadata in JSON format'
+    )
+    return parser
 
 
 def stem_cli_main():
-    pass  # TODO()
+    parser = create_parser()
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args.workspace, args)
+
+
+if __name__ == '__main__':
+    stem_cli_main()
