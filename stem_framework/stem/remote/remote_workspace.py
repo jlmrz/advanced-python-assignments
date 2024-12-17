f489c5e28214d737ecf260ec4e7c1105ff6a08f6
Julie Moroz <julie.prokasheva@gmail.com>
2024-12-17 17:25:34 +0300

    Assignment 10. Remote workspace and task

--- stem_framework/stem/remote/remote_workspace.py
+++ stem_framework/stem/remote/remote_workspace.py
@@ -1,30 +1,41 @@
 from typing import Any, TypeVar
 
-from stem.meta import Meta
+from stem.meta import Meta, get_meta_attr
 from stem.task import Task
 from stem.workspace import IWorkspace
 
 T = TypeVar("T")
 
+
 class RemoteTask(Task):
 
+    def __init__(self, address="localhost", port=8888, task_path: str = ''):
+        self.address = address
+        self.port = port
+        self.task_path = task_path
+
     def transform(self, meta: Meta, /, **kwargs: Any) -> T:
-        pass  # TODO(Assignment 10)
+        task = get_meta_attr(meta, 'workspace').find_task(meta['task_path'])
+        task_master = get_meta_attr(meta, 'task_master')
+        task_meta = get_meta_attr(meta, 'task_meta', {})
+        task_result_raw = task_master.execute(task_meta, task)
+        return task_result_raw
 
 
 class RemoteWorkspace(IWorkspace):
 
-    def __init__(self, address="localhost", port=8888):
+    def __init__(self, workspace: IWorkspace, address="localhost", port=8888):
         self.address = address
         self.port = port
+        self._workspace = workspace
 
     @property
     def tasks(self) -> dict[str, Task]:
-        pass  # TODO(Assignment 10)
+        return {task_: RemoteTask(self.address, self.port, task_) for task_ in self._workspace.tasks.keys()}
 
     @property
     def workspaces(self) -> set["IWorkspace"]:
-        pass  # TODO(Assignment 10)
+        return set([RemoteWorkspace(w, self.address, self.port) for w in self.workspaces])
 
 
 
